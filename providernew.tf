bdf5701ddae8ab382681c43dde259eafc186517b
Your Name <you@example.com>
2020-06-02 14:08:11 +0000

    ALB

---
+++ providernew.tf
@@ -0,0 +1,13 @@
+#Creates a VPC with cidr block 10.0.0.0/16
+provider "aws" {
+  access_key = "AKI"
+  secret_key = "ukb/y"
+  region     = "us-east-1"
+}
+
+#resource "aws_vpc" "myvpcnew" {
+# cidr_block = "10.0.0.0/16"
+# tags={
+#   Name = "myvpc"
+#   }
+#  }
